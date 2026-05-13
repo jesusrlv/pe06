77293a88f19203693c70ae0473d07dd562f104d1
jesusrlv <jesusrlvrojo@gmail.com>
2026-05-13 16:38:57 -0600

    datos de dashboard 2

--- sistema/admin/query/datosGraph.php
+++ sistema/admin/query/datosGraph.php
@@ -1,3 +1,12 @@
 <?php
 
 include('qc.php');
+
+$sql = "SELECT COUNT(*) as total FROM usr WHERE perfil = 1";
+$result = $conn->query($sql);
+$row = $result->fetch_assoc();
+$totalPostulantes = $row['total'];
+
+echo json_encode(['totalPostulantes' => $totalPostulantes]);
+
+?>
